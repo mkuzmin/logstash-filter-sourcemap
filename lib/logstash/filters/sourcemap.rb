bae5d32ac0972fa03aea2eecf94554ecd97416bc
Michael Kuzmin <mkuzmin@gmail.com>
2016-02-26 01:19:31 +0300

    Rename the plugin

---
+++ lib/logstash/filters/sourcemap.rb
@@ -0,0 +1,28 @@
+require 'logstash/filters/base'
+require 'logstash/namespace'
+
+class LogStash::Filters::SourceMap < LogStash::Filters::Base
+
+  config_name 'sourcemap'
+  
+  # Replace the message with this value.
+  config :message, :validate => :string, :default => "Hello World!"
+
+  public
+  def register
+    # Add instance variables 
+  end
+
+  public
+  def filter(event)
+
+    if @message
+      # Replace the event message with our message as configured in the
+      # config file.
+      event['error']['stack'] = @message
+    end
+
+    # filter_matched should go in the last line of our successful code
+    filter_matched(event)
+  end
+end
