5ce314d2bd9872ccd06d45885c2e27f90f81711b
Igor Shevchenko <cyberskunk@gmail.com>
2016-02-26 22:54:09 +0300

    add processed stack to the error report - `error.processedStack` array, described in the README.md

---
+++ docker/client/README.md
@@ -0,0 +1,17 @@
+## Example of processed stack
+```javascript
+[
+    {
+        "fn": "Object.docCookies.removeItem",
+        "file": "http://localhost:8080/cookies.min.js",
+        "line": "1",
+        "column": "883"
+    },
+    {
+        "fn": "HTMLInputElement.onclick",
+        "file": "http://localhost:8080/",
+        "line": "20",
+        "column": "99"
+    }
+]
+```
--- docker/client/report.js
+++ docker/client/report.js
@@ -3,19 +3,36 @@
 
     window.onerror = function (message, url, line, column, e) {
         oldHandler && oldHandler();
-        var xhr;
-      
-        xhr = new XMLHttpRequest();
-        xhr.open("POST", document.querySelector('.js_post-url').value || '//localhost/', true);
-        xhr.send(JSON.stringify({
-            project: 'buildserver',
-            error: {
+        var xhr,
+            error =  {
                 message: message,
                 url: url,
                 line: line,
                 column: column,
                 stack: e && e.stack || ''
-            },
+            };
+
+        if (e && e.stack) {
+            error.processedStack = e.stack
+                .split('\n')
+                .map(function (item) {
+                    var match = item.match(/^ *at ([^ ]*) \((.*):([0-9]+):([0-9]+)\)$/);
+                    return !match || {
+                        fn: match[1],
+                        file: match[2],
+                        line: match[3],
+                        column: match[4]
+                    };
+                })
+                .filter(function (item) { return typeof item == 'object'; });
+        }
+
+        xhr = new XMLHttpRequest();
+        xhr.open("POST", document.querySelector('.js_post-url').value || '//localhost/', true);
+
+        xhr.send(JSON.stringify({
+            project: 'buildserver',
+            error: error,
             doc: window.location.href,
             mapUrl: document.querySelector('.js_map-url').value
         }));
