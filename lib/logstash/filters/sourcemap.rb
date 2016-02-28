09ae49b0c87be7a182e6742817d83ac2d07fce3a
Michael Kuzmin <mkuzmin@gmail.com>
2016-02-28 05:18:20 +0300

    Load sourcemaps and rewrite offsets

--- lib/logstash/filters/sourcemap.rb
+++ lib/logstash/filters/sourcemap.rb
@@ -1,9 +1,13 @@
 require 'logstash/filters/base'
 require 'logstash/namespace'
+require 'open-uri'
+require 'sourcemap'
 
 class LogStash::Filters::SourceMap < LogStash::Filters::Base
 
   config_name 'sourcemap'
+
+  config :server, :validate => :string, :required => true
 
   public
   def register
@@ -17,8 +21,8 @@ class LogStash::Filters::SourceMap < LogStash::Filters::Base
     values = event['exception']['values']
     if values==nil || values.is_a?(Array)==false || values.length!=1 || values[0]['value']==nil
       @logger.warn('SourceMap filter cannot parse exception', :values => values)
-        event.tag('_sourcemapparsefailure')
-        return
+      event.tag('_sourcemapparsefailure')
+      return
     end
     exception = values.first
 
@@ -34,9 +38,12 @@ class LogStash::Filters::SourceMap < LogStash::Filters::Base
 
     if exception['stacktrace'] && exception['stacktrace']['frames']
       exception['stacktrace']['frames'].each do |frame|
-        url = URI.parse(frame['filename'])
-        if url.route_from(event['request']['url']).relative?
-          filename = url.path.to_s
+
+        remap!(frame)
+
+        script_url = URI.parse(frame['filename'])
+        if script_url.route_from(event['request']['url']).relative?
+          filename = script_url.path.to_s
         else
           filename = frame['filename']
         end
@@ -47,4 +54,41 @@ class LogStash::Filters::SourceMap < LogStash::Filters::Base
     event['stacktrace'] = message
     filter_matched(event)
   end
+
+  private
+  def remap!(frame)
+    script_url = URI.parse(frame['filename'])
+    return unless script_url.open.content_type == 'application/javascript'
+
+    map_filename = nil
+    script_url.open.each do |line|
+      if %r{^//# sourceMappingURL=(.+)} =~ line
+        map_filename = $1
+        break
+      end
+    end
+    return unless map_filename
+
+    map_url = URI.join(@server, map_filename)
+    map = SourceMap::Map.from_json(map_url.read)
+    return unless map
+
+    mapping = map.bsearch(SourceMap::Offset.new(frame['lineno'], frame['colno']))
+    return unless mapping
+
+    frame['raw_filename'] = frame['filename']
+    frame['filename'] = URI.join(frame['filename'], mapping.source).to_s
+
+    frame['raw_lineno'] = frame['lineno']
+    frame['lineno'] = mapping.original.line
+
+    frame['raw_colno'] = frame['colno']
+    frame['colno'] = mapping.original.column
+
+    if mapping.name
+      frame['raw_function'] = frame['function']
+      frame['function'] = mapping.name
+    end
+  end
+
 end
